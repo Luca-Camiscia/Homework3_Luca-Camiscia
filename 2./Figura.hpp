42607a712f5c1b8b8a0293dced11d1916698aea6
luca_camiscia <camiscialuca@gmail.com>
2025-05-16 10:43:50 -0300

    Update makefile to include C++20 standard; add main program demonstrating area calculations for Figura classes.

--- 2./Figura.hpp
+++ 2./Figura.hpp
@@ -8,14 +8,7 @@
 #include <concepts>
 using namespace std;
 constexpr double pi = 3.14159265358979323846;
-template<typename F>
-concept is_Pos = is_same_v<F,Posicion>;
-template<typename F>
-concept is_Cir = is_same_v<F,Circulo>;
-template<typename F>
-concept is_Eli = is_same_v<F,Elipse>;
-template<typename F>
-concept is_Rec = is_same_v<F,Rectangulo>;
+
 
 //Podria usar mas Using para las tuplas
 class Posicion{
@@ -58,25 +51,32 @@ class Rectangulo {
     tuple<Posicion,double, double> Get();
 };
 
+template<typename F>
+concept is_Pos = is_same_v<F,Posicion>;
+template<typename F>
+concept is_Cir = is_same_v<F,Circulo>;
+template<typename F>
+concept is_Eli = is_same_v<F,Elipse>;
+template<typename F>
+concept is_Rec = is_same_v<F,Rectangulo>;
+
+
 class ProcesadorFigura{
 public:
-    template<typename F>
-    static double Area(F f);
-
     template<is_Pos F>
-
-    
-
+    static double Area(F f){
+        return 0;
+    }
     // Especialización para Circulo
     template<is_Cir F>
-    static double Area(Circulo f) {
+    static double Area(F f) {
         auto [pos, radio] = f.Get();
         return pi * radio * radio;
     }
 
     // Especialización para Elipse
     template<is_Eli F>
-    static double Area(Elipse f) {
+    static double Area(F f) {
         auto [pos, semiejes] = f.Get();
         auto [a, b] = semiejes;
         return pi * a * b;
@@ -84,7 +84,7 @@ public:
 
     // Especialización para Rectangulo
     template<is_Rec F>
-    static double Area(Rectangulo f) {
+    static double Area(F f) {
         auto [pos, ancho, largo] = f.Get();
         return ancho * largo;
     }
---
+++ 2./main.cpp
@@ -0,0 +1,24 @@
+#include "Figura.hpp"
+
+
+int main(void) {
+    // Ejemplo con Posicion
+    Posicion pos1(4, 4);
+    Posicion pos2(2, 3);
+    Posicion pos3(0, 0);
+    Posicion pos4(5, 1);
+
+    // Ejemplo con Circulo
+    Circulo circulo(pos1, 2.0);
+    cout << "Area circulo = " << to_string(ProcesadorFigura::Area(circulo)) << endl;
+
+    // Ejemplo con Elipse
+    Elipse elipse(pos2, make_tuple(3.0,1.5));
+    cout << "Area elipse = " << to_string(ProcesadorFigura::Area(elipse)) << endl;
+
+    // Ejemplo con Rectangulo
+    Rectangulo rectangulo(pos3, 4.0, 6.0);
+    cout << "Area rectangulo = " << to_string(ProcesadorFigura::Area(rectangulo)) << endl;
+
+    return 0;
+}
